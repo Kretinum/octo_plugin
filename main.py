747cc24ca9c432c431e110f6c308561391c822e5
Kretinum <63955452+Kretinum@users.noreply.github.com>
2022-02-16 00:19:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, unicode_literals
+
+__plugin_name__ = "conn_plugin"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = ""
+__plugin_pythoncompat__ = ">=2.7,<4"
